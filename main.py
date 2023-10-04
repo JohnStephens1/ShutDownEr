ad9be7d8e20e767abd17239f403b6139cd7516fd
john <johnathan.stephens@protonmail.com>
2023-10-04 16:43:46 +0200

    clean up

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import os
+import time
+from sys import exit
+
 import tkinter as tk
 from tkinter import simpledialog
 from tkinter import messagebox
-import os
-import time
-
 
 # TODO
 #  add window post init_shutdown(), closing results in aborted shutdown
@@ -15,11 +16,14 @@ import time
 def get_fancy_time():
 	while True:
 		try:
-			fancy_time = tk.simpledialog.askstring("ShutDownEr", "Please enter a time:").replace(" ", "")
+			fancy_time = tk.simpledialog.askstring("ShutDownEr", "Please enter a time:")
 
 			if fancy_time is None:
 				exit()
-			elif fancy_time == "":
+
+			fancy_time = fancy_time.replace(" ", "")
+
+			if fancy_time == "":
 				return "0"
 			elif 1000000 > int(fancy_time) >= 0:
 				return fancy_time
@@ -27,14 +31,12 @@ def get_fancy_time():
 				raise ValueError
 		except ValueError:
 			tk.messagebox.showinfo("ShutDownEr", "Improper formatting.\n\nPlease enter up to six digits.")
-		except AttributeError:
-			exit()
 
 
 def fancy_time_to_seconds(fancy_time):
 	total_seconds = 0
 
-	for pos, digit in enumerate(reversed(f'{fancy_time}')):
+	for pos, digit in enumerate(reversed(f"{fancy_time}")):
 		total_seconds += int(digit) * 6 ** (pos // 2) * 10 ** ((pos + 1) // 2)
 
 	return total_seconds
@@ -51,30 +53,15 @@ def split_up_seconds(seconds):
 def seconds_to_time_string(seconds):
 	days, hours, minutes, seconds = split_up_seconds(seconds)
 
-	time_string = ''
+	time_string = ""
 	if days:
-		time_string = f'{days}d '
-	if hours or days:
-		time_string += f'{hours}h '
-	if minutes or hours or days:
-		time_string += f'{minutes}m '
-	time_string += f'{seconds}s'
-
-	return time_string
-
-
-def seconds_to_compact_time_string(seconds):
-	days, hours, minutes, seconds = split_up_seconds(seconds)
-
-	time_string = ''
-	if days:
-		time_string = f'{days}d'
+		time_string = f"{days}d"
 	if hours:
-		time_string += f'{hours}h'
+		time_string += f"{hours}h"
 	if minutes:
-		time_string += f'{minutes}m'
+		time_string += f"{minutes}m"
 	if seconds or not time_string:
-		time_string += f'{seconds}s'
+		time_string += f"{seconds}s"
 
 	return time_string
 
@@ -92,13 +79,12 @@ def initialize_shutdown(seconds, sleep):
 def main():
 	fancy_time = get_fancy_time()
 	seconds = fancy_time_to_seconds(fancy_time)
-	time_string = seconds_to_compact_time_string(seconds)
+	time_string = seconds_to_time_string(seconds)
 
 	sleep = tk.messagebox.askyesno("ShutDownEr", "pls chos yes 4 slep & no 4 shutzown pls im lazy sorrrieee D:")
 
 	if tk.messagebox.askyesno("ShutDownEr", f"Confirm {'sleep' if sleep else 'shutdown'} in {time_string} aka {seconds} seconds."):
 		initialize_shutdown(seconds, sleep)
-		print("not shutting down :)")
 	else:
 		tk.messagebox.showinfo("ShutDownEr", "GREATFUCKINGJOBMANWELLFUCKINGDOWNUGH")
 
@@ -113,11 +99,7 @@ def run_tests():
 	assert result_fancy_time_to_seconds == [0, 1, 5, 10, 60, 90, 90, 240, 5400], \
 		result_fancy_time_to_seconds
 
-	result_seconds_to_time_string = [seconds_to_time_string(x) for x in [0, 5, 60, 90, 3600, 3600 * 49]]
-	assert result_seconds_to_time_string == ['0s', '5s', '1m 0s', '1m 30s', '1h 0m 0s', '2d 1h 0m 0s'], \
-		result_seconds_to_time_string
-
-	result_seconds_to_compact_time_string = [seconds_to_compact_time_string(x) for x in [0, 5, 60, 90, 3600, 3600 * 49]]
+	result_seconds_to_compact_time_string = [seconds_to_time_string(x) for x in [0, 5, 60, 90, 3600, 3600 * 49]]
 	assert result_seconds_to_compact_time_string == ['0s', '5s', '1m', '1m30s', '1h', '2d1h'], \
 		result_seconds_to_compact_time_string
 
